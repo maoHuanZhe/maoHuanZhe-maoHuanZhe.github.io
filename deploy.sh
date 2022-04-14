a2ce2e48afb7083cd2572d791906418d5aa9aa74
fanguangrui <898365387@qq.com>
2022-04-14 10:56:23 +0800

    linux change file

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-msg='deploy'
-githubUrl=git@github.com:maoHuanZhe/maoHuanZhe.github.io.git
-
-git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:dist # 推送到github gh-pages分支
+git commit -m "linux change file"
+git push 
 
-cd -
-rm -rf docs/.vuepress/dist
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -120,14 +120,14 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2022, // 博客创建年份
       copyrightInfo:
-      '樊光瑞 | <a href="https://gitee.com/javaDaCaiNiao/book/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
+      '樊光瑞 | <a href="https://github.com/maoHuanZhe/maoHuanZhe.github.io/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
     extendFrontmatter: {
       author: {
         name: '樊光瑞',
-        link: 'https://gitee.com/javaDaCaiNiao'
+        link: 'https://github.com/maoHuanZhe'
       }
     },
 
