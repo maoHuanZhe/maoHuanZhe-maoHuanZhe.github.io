fd79db2638d0de20b5288f442631b2d0a0565a6d
maoHuanZhe <33337057+maoHuanZhe@users.noreply.github.com>
2022-04-15 14:07:16 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,6 +9,7 @@ import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
+  permalink: "/:year/:month/:day/:slug",
   theme: 'vdoing', // 使用npm主题包
   // theme: resolve(__dirname, '../../vdoing'), // 使用本地主题包
 
