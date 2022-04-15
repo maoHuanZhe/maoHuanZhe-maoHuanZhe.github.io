07f9bece5313d13d153e0cb8718af72b5bc496d1
fanguangrui <898365387@qq.com>
2022-04-15 08:29:10 +0800

    linux change file

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git pull gitee
+git pull gitee master
 
-git pull github
+git pull github master
 
 git add -A
 git commit -m "linux change file"
-git push gitee
+git push gitee master
 
-git push github
+git push github master
 
 
