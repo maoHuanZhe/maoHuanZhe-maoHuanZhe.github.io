2c2445e487c14746618a849dfd0727c1771749cd
fanguangrui <898365387@qq.com>
2022-04-20 22:00:00 +0800

    linux change config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -160,7 +160,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   // 插件配置
   plugins: <UserPlugins>[
-
+    'reading-progress',//阅读进度条
     'vuepress-plugin-baidu-autopush', // 百度自动推送
 
     [
