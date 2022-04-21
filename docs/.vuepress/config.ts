4a0609098079f8156dd4bb47051f281141f0a79e
fanguangrui <898365387@qq.com>
2022-04-21 16:41:22 +0800

    linux change file

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -165,12 +165,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     'vuepress-plugin-smooth-scroll',//平滑滚动
     'vuepress-plugin-baidu-autopush', // 百度自动推送
 
-    [
-      'vuepress-plugin-baidu-tongji', // 百度统计
-      {
-        hm: '6cf403304feb05442c04f21fee3306e4'
-      },
-    ],
+    ['vuepress-plugin-baidu-tongji-analytics', {
+        key: '6cf403304feb05442c04f21fee3306e4'
+    }],
 
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
     'fulltext-search',
